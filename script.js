87934f3dc6c0a9124f13b58cd6d745f25129881d
Doky37 <2213028634@correo.ler.uam.mx>
2025-04-15 09:45:42 -0600

    ksakd

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.letterSpacing = "10px";
+ctx.font="bold 40px serif";
+ctx.strokeText("Un texto en mi canvas", 5, 240);
+
+ctx.shadowColor = "red";
+ctx.shadowBlur = 15;
+
+ctx.font="italic 30px sans ";
+ctx.fillText("Otro texto en el canvas", 5, 280);
+
+ctx.shadowColor = "blue";
+ctx.shadowOffsetX = 10;
+ctx.shadowOffsetY =10;
+ctx.shadowBlur = 5;
+
+ctx.beginPath();
+
+ctx.arc(500,350,100,0,2*Math.PI);
+ctx.stroke();
+
